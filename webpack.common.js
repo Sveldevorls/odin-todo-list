bf87bd350dd6e67bc49ddb1fe234422239b33d3d
Sveldevorls <64679979+Sveldevorls@users.noreply.github.com>
2025-02-16 02:26:36 +0800

    Add template layout for sidebar

--- webpack.common.js
+++ webpack.common.js
@@ -20,7 +20,7 @@ module.exports = {
         use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.(png|jpg)$/i,
+        test: /\.(jpg|png|svg)$/i,
         type: "asset/resource",
       },
       {
